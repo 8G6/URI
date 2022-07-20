7506214fa3d49e83c4d3b4f04503232ee036cdfe
Devadut S Balan <56481296+8G6@users.noreply.github.com>
2022-07-20 18:41:28 +0530

    Update test.c

--- test.c
+++ test.c
@@ -2,7 +2,7 @@
 #include "URI.h"
 
 int main(){
-     char a[] = "&sxsrf=ALiCzsYq4Gh3tIkQwJfZOqpLDUJWDEUJug%3A1658257346998&ei=wv_WYtmPPIGOseMPqoiCsAQ&ved=0ahUKEwjZ-POA0oX5AhUBR2wGHSqEAEYQ4dUDCA4&uact=5&oq=Jun+as+male+name&gs_lcp=Cgdnd3Mtd2l6EAM6BwgAEEcQsANKBAhBGABKBAhGGABQ5gVY5gVg0ghoAXABeACAAbQBiAG0AZIBAzAuMZgBAKABAcgBCMABAQ&sclient=gws-wiz";
+     char a[] = "https://www.amazon.in/b/ref=cepc_sbc_atfbau_2?pf_rd_r=PQHSWM83Y12WA4TDASR0&pf_rd_p=fabbe388-be7c-435a-a144-6d2db918738f&pf_rd_m=A1VBAL9TL5WCBF&pf_rd_s=merchandised-search-3&pf_rd_t=30901&pf_rd_i=976419031&node=11599648031";
      char b[500];
      URI_JSON(a,b);
      write(b,"data.json","w");
